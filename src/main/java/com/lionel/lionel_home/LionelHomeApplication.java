55e489215a4b42c8d3339d4ab394a1a0b6d6a15f
guhongxing <1094163084@qq.com>
2018-12-13 15:57:18 +0800

    first commit

---
+++ src/main/java/com/lionel/lionel_home/LionelHomeApplication.java
@@ -0,0 +1,14 @@
+package com.lionel.lionel_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionelHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionelHomeApplication.class, args);
+    }
+
+}
+
